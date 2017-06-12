eeac0014bb1daf0a207c8b38abac2d533eeed016
Rafa Gómez <rgomezcasas@gmail.com>
2017-06-12 19:40:53 +0200

    Add redundant tests

--- src/AppBundle/Model/Event.php
+++ src/AppBundle/Model/Event.php
@@ -5,7 +5,6 @@ namespace AppBundle\Model;
 class Event
 {
     private $name;
-
     private $date;
 
     public function __construct($name, \DateTime $date)
@@ -22,5 +21,15 @@ class Event
     public function getDate()
     {
         return $this->date;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function setDate(\DateTime $date)
+    {
+        $this->date = $date;
     }
 }
